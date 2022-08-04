9400b91f08f25d5597896020a0befff1030019fe
illydali <ili.zaynal@gmail.com>
2022-08-05 00:08:12 +0800

    added currrency pipe

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { AddEmpComponent } from "./add-emp/add-emp.component";
 import { ListEmpComponent } from "./list-emp/list-emp.component";
+import { LoginComponent } from "./login/login.component";
 import { UpdateEmpComponent } from "./update-emp/update-emp.component";
 
 const routes: Routes = [
     // localhost:4200 (localhost:4200/employees)
-    {path : '', redirectTo: 'employees', pathMatch: 'full'},
+    {path : '', redirectTo: 'login', pathMatch: 'full'},
+    {path : 'login', component:LoginComponent},
     // employees = load ListEmpComponent
     {path: 'employees', component:ListEmpComponent},
     {path: 'add', component:AddEmpComponent},
